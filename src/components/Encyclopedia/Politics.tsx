3597849b7cf0a3d54b92542db3d6beb1b2934e2e
ltytgat <loantytgat@gmail.com>
2025-01-26 10:43:40 +0100

    Update Politics.tsx

--- src/components/Encyclopedia/Politics.tsx
+++ src/components/Encyclopedia/Politics.tsx
@@ -266,7 +266,100 @@ const articles = [
     title: 'Organisation interne de l\'Alliance',
     subtitle: 'Structure militaire',
     resume: 'L\'organisation interne de l\'Alliance, la force militaire unifiée.',
-    //sections: []
+    sections: [
+      {
+        title: 'Direction',
+        content: [
+          "L’Alliance est divisée en différentes structures, chacune dirigé par un amiral :",
+          "Division Opérationnelle, chargé de la gestion des Starlancer affiliés à la flotte ainsi que de l’attribution des missions courantes",
+          "Division Stratégique, chargé du développement de l’Alliance ainsi que de l’attribution des missions critiques",
+          "Division Logistique, chargé de l’approvisionnement des stations de l’Alliance et de la gestion des installations",
+          "Division Technologique, chargé de la conception du matériel et de la recherche dans le domaine militaire",
+          "L’Alliance est dirigé par l’amirauté, un conseil où l'on trouve les quatre amiraux dirigeant les quatre Divisions, ainsi que des vice-amiraux tirés au hasard parmi tous les vice-amiraux pour un durée de 4 <a href=\"#\" data-article=\"galactic-calendar\" class=\"text-blue-400 hover:text-blue-300\">cycles</a>, ainsi que le Premier Amiral de la flotte qui est par tradition l’amiral du Secteur Hélios.",
+          "Le QG de l’Alliance est basé au sein de la station spatiale <a href=\"#\" data-article=\"alcor\" class=\"text-blue-400 hover:text-blue-300\">Alcor</a> où il occupe une grande partie de la station et accueil tous les organismes décisionnaires de l’Alliance ainsi que certains de ses plus performants chantiers spatiaux.",
+          "L’Alliance divise l’espace de la <a href=\"#\" data-article=\"confederation\" class=\"text-blue-400 hover:text-blue-300\">Confédération</a> en secteurs regroupant 4 à 6 systèmes exploités. Chaque secteur est dirigé par un vice-amiral basé sur une station spatiale situé en orbite de la planète la plus développee du secteur. Ces stations font offices de quartier général régionale et regroupe tout le materiel nécessaire aux Starlancer de l’Alliance ou en cas de besoin aux autres. L’Alliance possède aussi de nombreux avant-postes situés dans les systèmes de chaque secteur ainsi que des garnisons sur les planètes en cours de colonisation ou de monde demandant une surveillance particulière (c’est le cas de certaines planètes rendu non-viable par la guerre contre les Torks).",
+          "Quand bien même les forces armées <a href=\"#\" data-article=\"stranis\" class=\"text-blue-400 hover:text-blue-300\">Strani</a> ont rejoint l’Alliance, celle-ci ne couvre pas les systèmes Stranis ou Vadas, chaque race gère la sécurité des ses systèmes indépendamment des autres.",
+          "Cette supervision des systèmes aliens ne serait utilisé qu’en temps de guerre."
+        ]
+      },
+      {
+        title: 'Divisions',
+        content: [
+          "La Division Opérationnel",
+          "La Division opérationnel est chargée d’accuser réception des demandes d’aides des gouvernements planétaires ainsi que des ordres de missions des stations de l’Alliance. Il s’agit de la Division la plus administrative de l’Alliance et possède au minimum un bureau dans toutes les colonies, même les avant-postes.",
+          "On retrouve au sein de cette Division un grand nombre de secrétaires et d’employés de bureau qui s’occupe du fonctionnement quotidien de l’Alliance ainsi que de jeunes officiers en début de carrière.",
+          "La Division Stratégique",
+          "La Division stratégique est chargée de planifier le développement de l’Alliance et, à ce titre, s’occupe de l’attribution des missions de premières importances. On retrouve au sein de cette Division aussi bien des stratèges, des théoriciens militaires, des diplomates ou des politiciens dont la mission est de s’assurer que l’Alliance puisse toujours répondre aux menaces pesant sur le Consortium.",
+          "De par sa fonction, il s’agit de la Division la plus petite et la plus prestigieuse de l’Alliance et on ne la retrouve qu’au sein des stations principales de chaque secteur.",
+          "La Division Logistique",
+          "La Division logistique est la seconde Division la plus importante en termes d’effectifs de l’Alliance. Elle s’occupe de la gestion, de l’entretient du matériel et des infrastructures de l’Alliance. C’est également elle qui se charge de l’approvisionnement de toutes les garnisons de l’Alliance et, de ce fait, elle possède au moins un bureau dans toutes celles-ci. Enfin, elle est chargée du fonctionnement de nombreux chantiers spatiaux de l’Alliance qui fournissent à ses Starlancer les vaisseaux nécessaire pour accomplir leurs missions.",
+          "La Division Technologique",
+          "La Division technologique est une ancienne sous division de la Division logistique, elle a gagné son indépendance afin de se détacher de la lourdeur administrative de celle-ci et pour pouvoir collaborer plus facilement avec ses homologues de la Guilde et de l’Union. Elle est chargée du développement technologique et scientifique de l’Alliance, allant de la recherche en physique théorique à la psychologie humaine et extraterrestre en situation d’isolement (domaine qui s’est révélé capital pour les longs voyages spatiaux).",
+          "Elle dispose de locaux dans les stations principales des secteurs de l’Alliance mais fonctionne principalement grâce à ses propres station-laboratoires.",
+          "La sous-division de garnison",
+          "La sous-section de garnison est une subdivision de la Division opérationnel chargé de la gestion des garnisons planétaire de l’Alliance, bien que très importante en terme d’effectif, elle ne constitue pas une Division propre du fait de son extrême dispersion et des liens étroits entre les garnisons et les gouvernements locaux.",
+          "Chaque garnison est gérée par un commandant, sa première tâche sera la formation des forces de sécurité locale."
+        ]
+      },
+      {
+        title: 'Catégorisation des missions',
+        content: [
+          "L’Alliance utilise un système de catégories pour classer les différentes missions qu’elle propose au Starlancer, cette catégorisation est menée par la division opérationnel mais la division stratégique peut se saisir de n’importe qu’elle mission.",
+          "Catégorie 1",
+          "Les missions de catégorie 1 sont les plus faciles que propose l’Alliance, il s’agit de mission très variables dans leur objectif (escorte de transport de marchandise ou de voyageur, élimination de faune hostile peu dangereuse, etc…) mais ne présentant pas de dangerosité particulière et ne demandant pas de compétence importante.",
+          "Ce genre de mission est très peu rémunéré mais sert souvent de départ pour les débutants et de bonus dans le cas d’un voyage.",
+          "Catégorie 2",
+          "Les missions de catégorie 2 sont les plus courantes, les objectifs de ce type de mission sont également très variable mais implique souvent de faire face à une menace particulière ou nécessite des compétences spécifiques (chasse au pirate, convoyage de matériel ou de personnel important, chasse à la faune dangereuse, destruction d’astéroïdes, etc.).",
+          "Ces missions sont d’avantage rémunéré mais attirent surtout des Starlancer moyennement expérimenté.",
+          "Catégorie 3",
+          "Les missions de catégorie 3 représentent les missions courantes présentant un niveau de dangers plus élevé ou une plus grande importance pour l’Alliance, elles sont aussi variable que celles de catégorie 2 mais nécessite des Starlancer plus expérimenté.",
+          "Ces missions sont bien rémunérées et attirent tout type de Starlancer expérimentés.",
+          "Catégorie 4",
+          "La catégorie 4 regroupe les missions difficiles de l’Alliance demandant la mobilisation de Starlancer chevronnés, on retrouve dans cette catégorie essentiellement des missions d’élimination de menace importante (bande de pirates, chef pirates, criminel de haut niveau, destruction d’installations, ect.).",
+          "Catégorie 5/Mission stratégique",
+          "La catégorie 5 est attribué aux missions fournis par la branche stratégique de l’Alliance, celles-ci sont souvent très difficiles et ont toujours des objectifs très spécifiques, voir exotique. Elles ne sont pas proposé sur les tableaux de missions mais c’est la division stratégique qui se charge de contacter des escouades à même de les accomplir, elles restent toutefois non-obligatoire mais la récompense est toujours à la hauteur de la difficulté.",
+          "Mission urgentes",
+          "Les missions urgentes sont aussi fournis par la division stratégique mais sont cette fois-ci sont obligatoire pour les Starlancer de l’Alliance, il s’agit de mobiliser un grand nombre de Starlancer pour faire face à une urgence.",
+          "Elles ne sont toutefois pas aussi difficiles que les missions de catégorie 5.",
+          "Ces missions sont en règle général bien rémunéré mais surtout, toute absence non justifiée peut faire l’objet de sanctions.",
+          "Remarque : La division stratégique sélectionne le type de Starlancer mobilisé ainsi des débutant ne risque pas d’être envoyé dans une mission suicidaire pour eux.",
+          "La re-catégorisation",
+          "Il arrive parfois que la branche opérationnel face une erreur dans la catégorisation d’une mission, dans ce cas, les Starlancer peuvent faire une demande (avec justification) pour que la récompense soit réévaluée après la mission.",
+          "Cette réévaluation ne peut se faire qu’à l’avantage des Starlancer (mission de catégorie 2 réévaluée en catégorie 3 avec augmentation de prime mais pas catégorie 2 vers catégorie 1 avec baisse de prime).",
+          "Des demandes de re-catégorisation trop fréquentes et injustifiées peuvent faire l’objet de sanction."
+        ]
+      },
+      {
+        title: 'Les Stranis dans l\'Alliance',
+        content: [
+          "Après plus de deux siècles de présence des Stranis dans l’Alliance, ceux-ci se sont parfaitement intégrés à cette organisation et la hiérarchie ne tient pratiquement plus compte des différences entre humain et Stranis.",
+          "De ce fait, on retrouve des membres de cette espèce en très grand nombre dans l’Alliance (bien qu’ils restent moins nombreux que les humains) et à tous les postes qu’elle peut proposer, y compris dans les plus hautes instances décisionnaires.",
+          "Malgré tout, les différences naturelles entre humains et Stranis font qu’il existe une tendance à encourager les membres d’un ou l’autre peuple dans certaines voies.",
+          "Étant donné qu’il n’existe pas de réelle différence entre les capacités cognitives des deux espèces, toutes les divisions sont ouvertes à part égale à leurs membres. Il existe toutefois une spécialisation forte des Stranis appartenant à la division logistique pour le travail de précision, le travail en environnement légèrement toxique et pour la maintenance où les caractéristiques naturelles des membres de cette espèce sont très supérieures à celles d'humains.",
+          "De plus, les Stranis ont tendance à préférer le travail en apesanteur où leur faiblesse physique est moins handicapante."
+        ]
+      },
+      {
+        title: 'Installations notables',
+        content: [
+          "Le Quartier Général d’Alcor",
+          "Le Quartier Général d’Alcor, souvent appelé QGA, est le plus haut lieu de commandement de l’Alliance, situé au sein de la station Alcor.",
+          "On retrouve au sein de ce quartier, la direction général de toutes les branches et divisions de l’Alliance, c’est également dans ce lieu que siège l’Amirauté, le conseil chargé de la direction de l’Alliance.",
+          "En plus d’être un gigantesque centre administratif, la station regroupe tous les services nécessaires aux Starlancer de l’Alliance, tel que des casernes, terrains d’entraînements, armureries, hangars, hôpitaux militaires, etc. Enfin, la station regroupe sous son commandement direct les chantiers spatiaux de l’Alliance dans le système Hélios.",
+          "Du fait de son importance, seuls les Starlancer affiliés à l’Alliance et les Starlancer spécifiquement invités ont le droit de se rendre dans les zones protégées du QG.",
+          "Le multi-complexe Laste",
+          "Le multi-complexe Laste est le regroupement administratif de tous les complexes de recherche de l’Alliance au sein du système Hélios.",
+          "Il consiste en de nombreuses stations et installations dans tout le système tel que (liste non exhaustive):",
+          "- Laste, se trouvant sur Deimos, qui archive les recherches du complexe.",
+          "- Arès, sur Phobos, il s’agit de la principale station du complexe et regroupe les recherches en matière d’armement ainsi que de nombreux projets exotiques.",
+          "- Vénus, en orbite de cette planète, qui étudie le comportement des vaisseaux et des équipements en condition climatique extrême.",
+          "- Amalthée, sur la lune éponyme de Jupiter, qui teste le matériel de l’Alliance dans la grande variété de condition que sont les lunes de Jupiter.",
+          "- Cérès, situé dans la ceinture d’astéroïde intérieur du système Hélios, elle étudie les systèmes de navigation et de combat en utilisant la ceinture d’astéroïde comme terrain d’essai.",
+          "La base Vigilance",
+          "Il s’agit de l’une des plus grande base de l’Alliance car son rôle est de surveiller Cerberi et d’assurer l’influence de la Confédération au niveau de la frontière. Le commandement de cette base est l’un des postes les plus critique de l’Alliance du fait de son côté très sensible."
+        ]
+      }
+    ]
   },
 ];
 
