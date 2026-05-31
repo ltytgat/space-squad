2667e3136c7b3fcd84be0eb72605e809f22d614a
ltytgat <loantytgat@gmail.com>
2026-05-31 14:42:53 +0200

    Fix turbopack

--- next.config.ts
+++ next.config.ts
@@ -24,9 +24,6 @@ const nextConfig: NextConfig = {
 
     return webpackConfig
   },
-  turbopack: {
-    root: path.resolve(dirname),
-  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
